b411e08035b5a786006c610e0764496a65a7b875
MikhBohdan <114218353+MikhBohdan@users.noreply.github.com>
2023-01-27 21:40:12 +0100

    Add files via upload

---
+++ souborna prace/souborna pracr/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+bool is_valid(string input, string soustava)
+{
+    return false;
+}
+
+
+{
+    Console.WriteLine("Ahoj!Vás vitá převaděč");
+    while (true)
+    {
+        string[] soustavy = { "2", "8", "16" };
+        Console.Write("zadej cislo soustavy: ");
+        string soustava = Console.ReadLine();
+
+        if (!soustavy.Contains(soustava))
+        {
+            // spatna soustava
+            Console.WriteLine("Spatna soustava");
+            break;
+        }
+
+        Console.Write("zadej hodnotu soustavy: ");
+        string hodnota = Console.ReadLine();
+
+
+        try
+        {
+            Console.WriteLine("vložene číslo {0} v {1} soustavě = {2} desítkově", hodnota, soustava,( Convert.ToInt64(hodnota, Int32.Parse(soustava))));
+        }
+        catch
+        {
+            //  spatna hodnota
+            Console.WriteLine("Spatna hodnota");
+            break;
+        }
+    }  
+}
